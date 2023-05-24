ddeec8e51acb6d8d7b636663d0db2a3448efad86
gustavolara11 <gustavex2007@gmail.com>
2023-05-24 11:31:11 -0300

    feat: implement add and delete funcions

--- pages/Agenda/index.js
+++ pages/Agenda/index.js
@@ -1,19 +1,64 @@
 const openModal = () => {
-  const openMod = document.getElementById("modal-content");
-  openMod.style.display = "flex";
+  const modal = document.getElementById("modal-content");
+  modal.style.display = "flex";
 };
 
 const closeModal = () => {
-  const closeMod = document.getElementById("modal-content");
-  closeMod.style.display = "none";
+  const modal = document.getElementById("modal-content");
+  modal.style.display = "none";
 };
+
+const agenda = [];
 
 const newAppoint = () => {
-  let pcte = {
-    name: document.getElementById("name-add").value,
-    date: document.getElementById("hour-add").value,
+  const name = document.getElementById("name-add").value;
+  const date = document.getElementById("date-add").value;
+  const hour = document.getElementById("hour-add").value;
+
+  const appointment = {
+    name: name,
+    date: date,
+    hour: hour,
   };
-  localStorage.setItem("pcte", JSON.stringify(pcte));
+
+  agenda.push(appointment);
+
+  const table = document.getElementById("table1");
+
+  const newRow = document.createElement("tr");
+
+  const newHour = document.createElement("td");
+  newHour.textContent = hour;
+
+  const newName = document.createElement("td");
+  newName.textContent = name;
+
+  const newActions = document.createElement("td");
+
+  const editButton = document.createElement("button");
+  editButton.className = "button-2";
+  editButton.textContent = "Alterar";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "button-2";
+  deleteButton.textContent = "Excluir";
+  deleteButton.addEventListener("click", () => {
+    deleteAppointment(newRow);
+  });
+
+  newActions.appendChild(editButton);
+  newActions.appendChild(deleteButton);
+
+  newRow.appendChild(newHour);
+  newRow.appendChild(newName);
+  newRow.appendChild(newActions);
+
+  table.appendChild(newRow);
+
+  closeModal();
 };
 
-// let pcte = JSON.parse(localStorage.getItem("pcte"));
+const deleteAppointment = (row) => {
+  const table = document.getElementById("table1");
+  table.removeChild(row);
+};
